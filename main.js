19f6d008abd0e810b995b5f5a043126db1c6f926
Dmitry-Zverev <42356278+Dmitry-Zverev@users.noreply.github.com>
2018-12-04 21:22:47 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+//= ../../../node_modules/jquery/dist/jquery.js
+//= ../../../node_modules/popper.js/dist/umd/popper.js
+//= ../../../node_modules/bootstrap/js/dist/util.js
+//= ../../../node_modules/bootstrap/js/dist/tooltip.js
+//= ../../../node_modules/bootstrap/js/dist/collapse.js
+//= ../../../node_modules/bootstrap/js/dist/scrollspy.js
+//= ../../../node_modules/bootstrap/js/dist/dropdown.js
+//= ../../../node_modules/bootstrap/js/dist/button.js
+//= ../../../node_modules/bootstrap/js/dist/modal.js
+//= ../../../node_modules/owl.carousel/dist/owl.carousel.min.js
+
+
+
+
+
+
+
+//= fontawesome-all.js
+//= menu.js
+//= owl.carousel.js
